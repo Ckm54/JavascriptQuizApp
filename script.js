0c036f1643028d5af952875f67d6f8770025e927
Collo <mutaic84@gmail.com>
2022-02-06 11:04:44 +0300

    Adds click functionality to submit button to navigate between quizes

--- script.js
+++ script.js
@@ -34,23 +34,23 @@ const quizData = [
 
 ];
 
-const quiz = document.getElementById('quiz')
-const answer = document.querySelectorAll('.answer')
-const questionEl = document.getElementById('question')
-const a_text = document.getElementById('a_text')
-const b_text = document.getElementById('b_text')
-const c_text = document.getElementById('c_text')
-const d_text = document.getElementById('d_text')
-const e_text = document.getElementById('e_text')
-const submitBtn = document.getElementById('submit')
+const quiz = document.getElementById('quiz');
+const answer = document.querySelectorAll('.answer');
+const questionEl = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const e_text = document.getElementById('e_text');
+const submitBtn = document.getElementById('submit');
 
-let currentQuiz = 0
-let score = 0
+let currentQuiz = 0;
+let score = 0;
 
-loadQuiz()
+loadQuiz();
 
 function loadQuiz() {
-    deselectAnswers()
+    deselectAnswers();
 
     const currentQuizData = quizData[currentQuiz]
 
