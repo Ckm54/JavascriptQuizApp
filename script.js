0fdbbe5f7bd0b28dd4ce970e08ca3de83b516ee2
Collo <mutaic84@gmail.com>
2022-02-06 10:29:41 +0300

    define constants and variables to be used in manipulating and accessing answres chosen

--- script.js
+++ script.js
@@ -34,3 +34,16 @@ const quizData = [
 
 ];
 
+const quiz = document.getElementById('quiz')
+const answer = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const e_text = document.getElementById('e_text')
+const submitBtn = document.getElementById('submit')
+
+let currentQuiz = 0
+let score = 0
+
