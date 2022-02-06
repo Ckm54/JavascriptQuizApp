44cf1b0792ac6da4d3a93cb04c6c7b9a7b3a1925
Collo <mutaic84@gmail.com>
2022-02-06 10:28:10 +0300

    add the questions data as a list of objects

---
+++ script.js
@@ -0,0 +1,36 @@
+const quizData = [
+    {
+        question: "Who invented JavaScript?",
+        a: "Douglas Crockford",
+        b: "Sheryl Sandberg",
+        c: "Brendan Eich",
+        d : "Bill Gates",
+        correct: "c",
+    },
+    {
+        question: " Which type of language is javascript?",
+        a: "Object-Oriented",
+        b: "Object-Based",
+        c: "Assembly-language",
+        d: "High-level",
+        correct: "b",
+    },
+    {
+        question: "In HTML elements,CSS can be added in different",
+        a: "2 ways",
+        b: "1 way",
+        c: "3 ways",
+        d: "4 ways",
+        correct: "c",
+    },
+    {
+        question: "What does CSS stand for?",
+        a: "Cascading Style Sheets",
+        b: "Central Style Sheets",
+        c: "Cascading Simple Sheets",
+        d: "Cars SUVs Sailboats",
+        correct: "a",
+    },
+
+];
+
